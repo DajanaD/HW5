1bd810dc9afd850da5ef540d38f978755d3536f2
DajanaD <117541907+DajanaD@users.noreply.github.com>
2024-02-26 20:09:23 +0200

    pars main.py

--- main.py
+++ main.py
@@ -1,53 +1,54 @@
-import platform
-from datetime import datetime, timedelta
-from shlex import join
-import aiohttp
 import asyncio
+import aiohttp
+from datetime import datetime, timedelta
 import logging
+import platform
 
 EUR = {'EUR': {}}
 USD = {'USD': {}}
 list_currency = []
 
 
-# async def period():
-#     time_now =datetime.now()
-#     for el in range():
-#         current_time = time_now + timedelta(days=el)
-#         current_time.strftime("%d.%m.%Y")
+async def fetch_exchange_rates(session, date):
+    async with session.get(f'https://api.privatbank.ua/p24api/exchange_rates?date={date}') as response:
+        return await response.json()
 
-async def main(num):    
-    time_now =datetime.now()
-    if num >10:    # проверка на превышение 10 дней
-       return logging.info('request exceeds 10 days')
-    for el in range(num):    # итерация по дням
+
+async def parse_currency(date):
+    async with aiohttp.ClientSession() as session:
+        try:
+            result = await fetch_exchange_rates(session, date)
+            if 'exchangeRate' in result:
+                list_exchangeRate = result['exchangeRate']
+                eur_info = list(filter(lambda num: num['currency'] == 'EUR', list_exchangeRate))
+                EUR['EUR'] = {'sale': list(map(lambda x: x['saleRate'], eur_info)),
+                              'purchase': list(map(lambda x: x['purchaseRate'], eur_info))}
+                usd_info = list(filter(lambda num: num['currency'] == 'USD', list_exchangeRate))
+                USD['USD'] = {'sale': list(map(lambda x: x['saleRate'], usd_info)),
+                              'purchase': list(map(lambda x: x['purchaseRate'], usd_info))}
+                list_currency.append({date: {**EUR, **USD}})
+        except aiohttp.ClientConnectorError as err:
+            print(f'Connection error: {err}')
+
+
+async def main(num):
+    time_now = datetime.now()
+
+    if num > 10:
+        logging.info('Request exceeds 10 days')
+        return list_currency
+
+    tasks = []
+    for el in range(num):
         current_time = time_now - timedelta(days=el)
         current_time = current_time.strftime("%d.%m.%Y")
-   
-        async with aiohttp.ClientSession() as session: # получчение данных с сайта
-            try:
-                async with session.get(f'https://api.privatbank.ua/p24api/exchange_rates?date={current_time}') as response:
-                    result = await response.json()
-                    # return result
-                    for key, value in result.items(): # формирование списка словарей
-                        if key == 'exchangeRate':
-                            list_exchangeRate = value
-                            eur_info = list(filter(lambda num: num['currency'] == 'EUR', list_exchangeRate))
-                            EUR['EUR'] = {'sale':list(map(lambda x : x['saleRate'], eur_info)), 'purchase':list(map(lambda x : x['purchaseRate'], eur_info))}
-                            usd_info = list(filter(lambda num: num['currency'] == 'USD', list_exchangeRate))
-                            USD['USD'] = {'sale':list(map(lambda x : x['saleRate'], usd_info)), 'purchase':list(map(lambda x : x['purchaseRate'], eur_info))}
-                            # for el in EUR['EUR']:
-                                # el['sale'] = [float(x) for x in el['sale']]
-                            list_currency.append({current_time:{**EUR, **USD}})
-                return list_currency
-            except aiohttp.ClientConnectorError as err:
-                print(f'Connection error', str(err))
-                  
-                    
-        
+        tasks.append(parse_currency(current_time))
+
+    await asyncio.gather(*tasks)
+
 
 if __name__ == "__main__":
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main(11))
-    print(r)
+    asyncio.run(main(11))
+    print(list_currency)
